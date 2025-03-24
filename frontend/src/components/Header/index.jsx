2f3825c697a1cea135ebda3cfe253f3ed65f5ded
LucasBondeDeSouza <bondelucas77@gmail.com>
2025-03-24 12:15:07 -0300

    atualização

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,18 +1,21 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 import SearchIcon from '@mui/icons-material/Search';
 import HomeIcon from '@mui/icons-material/Home';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 
 export default () => {
-    const [selected, setSelected] = useState(true)
+    const [selected, setSelected] = useState(true);
 
     const handleSelect = () => {
-        setSelected(!selected)
-    }
+        setSelected(!selected);
+    };
 
     return (
-        <div className="flex items-center p-2 space-x-2">
-            <div className="bg-neutral-800 hover:bg-neutral-600 p-2 rounded-full cursor-pointer transition duration-500" onClick={() => handleSelect()}>
+        <div className="flex items-center p-2 space-x-2 max-w-6xl mx-auto">
+            <div 
+                className="flex items-center justify-center bg-neutral-800 hover:bg-neutral-700 h-12 w-12 rounded-full cursor-pointer transition duration-500" 
+                onClick={handleSelect}
+            >
                 {selected ? (
                     <HomeIcon fontSize="large" className="text-white" />
                 ) : (
@@ -20,13 +23,14 @@ export default () => {
                 )}
             </div>
 
-            <input 
-                className="bg-neutral-800 text-white rounded-full py-2 px-12 text-lg w-2/3 md:w-1/3 border-2 border-transparent focus:border-white outline-none transition duration-300" 
-                type="text"
-                placeholder="Pesquise um artista"
-            />
-
-            <SearchIcon className="absolute left-20 text-neutral-400" />
+            <div className="relative flex-grow min-w-0 max-w-xl md:w-2/4">
+                <input 
+                    className="bg-neutral-800 hover:bg-neutral-700 focus:bg-neutral-700 text-white rounded-full py-2 pl-12 text-lg w-full border-2 border-transparent focus:border-white outline-none transition duration-300" 
+                    type="text"
+                    placeholder="Pesquise um artista"
+                />
+                <SearchIcon className="absolute left-4 top-1/2 transform -translate-y-1/2 text-neutral-400" />
+            </div>
         </div>
-    )
-}
+    );
+};
