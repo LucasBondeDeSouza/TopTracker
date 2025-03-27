3a0631fa4cd465474b78e73287274d5a84b8c620
LucasBondeDeSouza <bondelucas77@gmail.com>
2025-03-27 12:43:36 -0300

    atualização

--- frontend/src/components/DatasArtist/index.jsx
+++ frontend/src/components/DatasArtist/index.jsx
@@ -15,21 +15,22 @@ export default ({ token, selectArtist }) => {
     }, [selectArtist]);
 
     return (
-        <div className="bg-neutral-900 rounded-lg sticky top-2">
+        <div className="sticky top-2">
             {data && (
-                <div className="relative">
-                    <img
-                        src={data.image}
-                        alt={data.name}
-                        className="h-100 w-full rounded-lg object-cover"
-                    />
-
-                    <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-tr from-black/50 to-transparent">
-                        <p className="text-white text-xl font-bold absolute bottom-5 left-5">
-                            {data.name}
-                        </p>
+                <>
+                    <div
+                        className="h-100 w-full rounded-lg bg-cover relative"
+                        style={{ backgroundImage: `url(${data.image})`}}
+                    >
+                        <div className="w-full h-full rounded-lg gradient bg-gradient-to-b to-[#111] from-transparent">
+                            <div className="w-full h-full rounded-lg gradient bg-gradient-to-l to-[#111] from-transparent">
+                                <p className="absolute bottom-10 left-10 text-white text-2xl">
+                                    {data.name}
+                                </p>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                </>
             )}
         </div>
     )
