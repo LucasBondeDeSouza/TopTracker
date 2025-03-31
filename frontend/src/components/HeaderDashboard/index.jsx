8b32008e2afb366c5bb5ab77d84307028cb43adb
LucasBondeDeSouza <bondelucas77@gmail.com>
2025-03-31 12:57:08 -0300

    atualização

--- frontend/src/components/HeaderDashboard/index.jsx
+++ frontend/src/components/HeaderDashboard/index.jsx
@@ -4,9 +4,9 @@ export default ({ selectCategory, setSelectCategory }) => {
 
     return (
         <div className="space-x-2 bg-neutral-900 rounded-t-lg py-2 px-10 z-10">
-            <button onClick={() => setSelectCategory('artists')} className={`py-2 px-4 text-sm cursor-pointer transition duration-300 rounded-full ${selectCategory == 'artists' ? 'bg-green-500 hover:bg-green-600' : 'bg-neutral-700 hover:bg-neutral-600 text-gray-100 font-bold'}`}>Artists</button>
-            <button onClick={() => setSelectCategory('songs')} className={`py-2 px-4 text-sm cursor-pointer transition duration-300 rounded-full ${selectCategory == 'songs' ? 'bg-green-500 hover:bg-green-600' : 'bg-neutral-700 hover:bg-neutral-600 text-gray-100 font-bold'}`}>Songs</button>
-            <button onClick={() => setSelectCategory('playlists')} className={`py-2 px-4 text-sm cursor-pointer transition duration-300 rounded-full ${selectCategory == 'playlists' ? 'bg-green-500 hover:bg-green-600' : 'bg-neutral-700 hover:bg-neutral-600 text-gray-100 font-bold'}`}>Playlists</button>
+            <button onClick={() => setSelectCategory('artists')} className={`py-2 px-4 text-sm cursor-pointer transition duration-300 rounded-full ${selectCategory == 'artists' ? 'bg-green-500' : 'bg-neutral-700 hover:bg-neutral-600 text-gray-100 font-bold'}`}>Artists</button>
+            <button onClick={() => setSelectCategory('songs')} className={`py-2 px-4 text-sm cursor-pointer transition duration-300 rounded-full ${selectCategory == 'songs' ? 'bg-green-500' : 'bg-neutral-700 hover:bg-neutral-600 text-gray-100 font-bold'}`}>Songs</button>
+            <button onClick={() => setSelectCategory('playlists')} className={`py-2 px-4 text-sm cursor-pointer transition duration-300 rounded-full ${selectCategory == 'playlists' ? 'bg-green-500' : 'bg-neutral-700 hover:bg-neutral-600 text-gray-100 font-bold'}`}>Playlists</button>
         </div>
     )
 }
