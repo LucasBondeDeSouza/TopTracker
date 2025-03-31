8d535533bcccd2e4cbef3eb4952e4560258f9939
LucasBondeDeSouza <bondelucas77@gmail.com>
2025-03-31 12:59:18 -0300

    atualização

--- frontend/src/components/DetailsArtist/index.jsx
+++ frontend/src/components/DetailsArtist/index.jsx
@@ -4,7 +4,7 @@ import CategoryDashboard from "../CategoryDashboard";
 export default ({ musics, albums }) => {
 
     return (
-        <div className="py-3 rounded-lg w-full bg-neutral-900">
+        <div className="py-3 rounded-lg w-full bg-neutral-900 mb-5">
             <CategoryDashboard title={'Músicas'} data={musics} size={'md'} />
             <CategoryDashboard title={'Albums'} data={albums} size={'md'} />
         </div>
