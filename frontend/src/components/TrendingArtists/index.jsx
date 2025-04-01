54693a9679ee16babe8abcda8ce85e69d83d6f9c
LucasBondeDeSouza <bondelucas77@gmail.com>
2025-04-01 12:48:24 -0300

    atualização

--- frontend/src/components/TrendingArtists/index.jsx
+++ frontend/src/components/TrendingArtists/index.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { fetchArtistsByGenre } from "../../SpotifyDB";
 import CategorySection from "../CategorySection";
+import SkeletonCategoryCard from "../SkeletonCategoryCard";
 
 export default ({ token, setSelectArtist }) => {
+    const [isLoading, setIsLoading] = useState(true)
     const [artistsPop, setArtistsPop] = useState([]);
     const [artistsSertanejo, setArtistsSertanejo] = useState([]);
     const [artistsFunk, setArtistsFunk] = useState([]);
@@ -12,24 +14,26 @@ export default ({ token, setSelectArtist }) => {
 
     useEffect(() => {
         const fetchData = async () => {
+            setIsLoading(true)
             setArtistsPop(await fetchArtistsByGenre(token, 'pop', 'US'));
             setArtistsSertanejo(await fetchArtistsByGenre(token, 'sertanejo', 'BR'));
             setArtistsFunk(await fetchArtistsByGenre(token, 'funk', 'BR'));
             setArtistsRock(await fetchArtistsByGenre(token, 'rock', 'BR'));
             setArtistsKPop(await fetchArtistsByGenre(token, 'k-pop', ''));
             setArtistsPagode(await fetchArtistsByGenre(token, 'pagode', 'BR'));
+            setIsLoading(false)
         };
         fetchData();
     }, [token]);
 
     return (
         <>
-            <CategorySection title={'Top Pop Artists'} data={artistsPop} setSelectArtist={setSelectArtist} size={'lg'} />
-            <CategorySection title={'Top Sertanejo Artists'} data={artistsSertanejo} setSelectArtist={setSelectArtist} size={'lg'} />
-            <CategorySection title={'Top Funk Artists'} data={artistsFunk} setSelectArtist={setSelectArtist} size={'lg'} />
-            <CategorySection title={'Top Rock Artists'} data={artistsRock} setSelectArtist={setSelectArtist} size={'lg'} />
-            <CategorySection title={'Top K-Pop Artists'} data={artistsKPop} setSelectArtist={setSelectArtist} size={'lg'} />
-            <CategorySection title={'Top Pagode Artists'} data={artistsPagode} setSelectArtist={setSelectArtist} size={'lg'} />
+            {isLoading ? <SkeletonCategoryCard /> : <CategorySection title={'Top Pop Artists'} data={artistsPop} setSelectArtist={setSelectArtist} size={'lg'} />}
+            {isLoading ? <SkeletonCategoryCard /> : <CategorySection title={'Top Sertanejo Artists'} data={artistsSertanejo} setSelectArtist={setSelectArtist} size={'lg'} />}
+            {isLoading ? <SkeletonCategoryCard /> : <CategorySection title={'Top Funk Artists'} data={artistsFunk} setSelectArtist={setSelectArtist} size={'lg'} />}
+            {isLoading ? <SkeletonCategoryCard /> : <CategorySection title={'Top Rock Artists'} data={artistsRock} setSelectArtist={setSelectArtist} size={'lg'} />}
+            {isLoading ? <SkeletonCategoryCard /> : <CategorySection title={'Top K-Pop Artists'} data={artistsKPop} setSelectArtist={setSelectArtist} size={'lg'} />}
+            {isLoading ? <SkeletonCategoryCard /> : <CategorySection title={'Top Pagode Artists'} data={artistsPagode} setSelectArtist={setSelectArtist} size={'lg'} />}
         </>
     );
 };
