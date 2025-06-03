70bb5679c7ab52d29da694e5e6fed92662343107
LucasBondeDeSouza <bondelucas77@gmail.com>
2025-06-03 15:59:47 -0300

    atualização

--- frontend/src/components/CategorySection/index.jsx
+++ frontend/src/components/CategorySection/index.jsx
@@ -48,15 +48,21 @@ export default ({ title, data = [], setSelectArtist = () => {}, size }) => {
                         <div className={`${imgSize} bg-cover rounded-lg mb-2`} style={{ backgroundImage: `url(${item.image})`}}></div>
                         
                         {item.artist && (
-                            <p className='text-neutral-50 text-sm font-semibold truncate'>{item.artist}</p>
+                            <p className="text-neutral-50 text-sm font-semibold">
+                                {item.artist.length > 18 ? item.artist.slice(0, 18) + "..." : item.artist}
+                            </p>
                         )}
 
                         {item.name && (
-                            <p className='text-neutral-400 text-sm font-semibold truncate'>{item.name}</p>
+                            <p className="text-neutral-400 text-sm font-semibold">
+                                {item.name.length > 18 ? item.name.slice(0, 18) + "..." : item.name}
+                            </p>
                         )}
 
                         {item.playlist && (
-                            <p className='text-white text-sm font-semibold truncate'>{item.playlist}</p>
+                            <p className="text-white text-sm font-semibold">
+                                {item.playlist.length > 30 ? item.playlist.slice(0, 30) + "..." : item.playlist}
+                            </p>
                         )}
                     </Link>
                 ))}
